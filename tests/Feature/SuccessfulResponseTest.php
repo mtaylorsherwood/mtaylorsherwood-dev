48f343ac6866af589bdcbddacbe614385c0895c8
Martyn Taylor-Sherwood <martyn@taylorsherwood.co.uk>
2024-06-29 09:01:23 +0100

    Rename of basic test example

--- tests/Feature/SuccessfulResponseTest.php
+++ tests/Feature/SuccessfulResponseTest.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class SuccessfulResponseTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic test for the app response.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
