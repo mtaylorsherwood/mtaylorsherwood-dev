dbef52cdfa9607557c325c6349011f46bef674fc
Martyn Taylor-Sherwood <martyn@taylorsherwood.co.uk>
2025-07-17 12:00:38 +0100

    Local development improvements

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,5 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-    ],
-    server: {
-        cors: {
-            origin: 'http://mtaylorsherwood-dev.test'
-        },
-    },
+    ]
 });
